276990d55e336240cbeb7fe128aeb1a5b1a471b5
CosBett <cosmasbett9@gmail.com>
2022-03-23 23:49:06 +0300

    create neighbourhood class

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Hood(models.Model):
+    name = models.CharField(max_length=150, blank=False)
+    location = models.CharField(max_length=100)
+    occupants_Count = models.ImageField(upload_to='images/')
+    admin = models.ForeignKey("User", on_delete=models.CASCADE, related_name='ad')
+    description = models.TextField(max_length=500, blank=False)
+    photo = models.ImageField(upload_to='images/')
+    police_contact = models.ImageField(upload_to='images/')
+    Health_contact = models.IntegerField(null=True, blank=True)
