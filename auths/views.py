7d25397cb68736a8ed9ef3789b434be188bfac37
CosBett <cosmasbett9@gmail.com>
2022-03-24 01:22:57 +0300

    create base template for registration and signing in

--- auths/views.py
+++ auths/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+def log_in(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in Sucessfully!")	
+            return redirect('homepage')
+        else:
+            messages.success(request, "There Was An Error Logging In, Try Again...")	
+            return redirect('login')
+    else:
+		    return render(request, 'registration/login.html', {})
