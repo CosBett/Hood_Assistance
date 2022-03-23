f65ae2770f39d29f35c78946536fa7edbda252f8
CosBett <cosmasbett9@gmail.com>
2022-03-24 02:04:19 +0300

    create signup endpoint template

--- auths/urls.py
+++ auths/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
     path('', include('django.contrib.auth.urls')),
     ]
