12d318048cac6544af238d65ffa98b26d4bced38
CosBett <cosmasbett9@gmail.com>
2022-03-24 03:46:10 +0300

    register model class in admin.py

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from . models import Profile,Hood,Post,Business
+
 def landing_page(request):
     pass
     return render(request,'landing_page.html', {}) 
 def index(request):
-    pass
     return render(request, 'index.html', {} )
